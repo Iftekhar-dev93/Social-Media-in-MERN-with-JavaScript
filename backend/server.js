665f60e0fcdbc1577391df3f2e18823e5157402c
unknown <iftekhar.dev93@gmail.com>
2025-12-22 18:59:08 +0600

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true}))
 
 app.get('/', (req, res) => {
     res.send(`
