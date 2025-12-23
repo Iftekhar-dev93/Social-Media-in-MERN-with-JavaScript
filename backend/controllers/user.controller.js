02764756d819cfddd0cd5551cde9e4fc38c6aff2
unknown <iftekhar.dev93@gmail.com>
2025-12-23 23:48:07 +0600

    user controller & auth middleware modified

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+
+export const getUserData = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.json({
+                success: false,
+                message: 'User not found'
+            })
+        }
+
+        res.json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+export const protect = async (req, res, next) => {
+    try {
+        const { userId } = await req.auth()
+
+        if (!userId) {
+            return res.json({
+                success: false,
+                message: 'User not authenticated'
+            })
+        }
+
+        next()
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
