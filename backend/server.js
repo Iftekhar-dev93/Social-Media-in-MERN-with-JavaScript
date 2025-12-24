96fa8d3b917921b72b2ae212e64ac9f4bd565048
unknown <iftekhar.dev93@gmail.com>
2025-12-24 23:54:21 +0600

    file modified

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import connectDB from './config/db.config.js'
+import connectDB from './configs/db.config.js'
 
 dotenv.config()
 
